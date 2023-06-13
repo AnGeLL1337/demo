b6f027e5396dcfa7b2bacab08c32cd456d96d471
CptDunk <vojtech.martinat@gmail.com>
2023-06-13 20:19:49 +0200

    Reuploading matej's component(faulty merge)

--- src/components/LessonTableRow.js
+++ src/components/LessonTableRow.js
@@ -2,7 +2,8 @@ import {DeleteButton} from "components/DeleteButton";
 import {LessonUserSelectBox} from "components/LessonUserSelectBox";
 import {LessonFacilitySelectBox} from "components/LessonFacilitySelectBox";
 import {LessonTypeSelectBox} from "components/LessonTypeSelectBox";
-
+import UpdateLessonOrderButton from "./UpdateLessonOrderButton";
+import {useState} from "react";
 
 /**
  * Komponenta reprezentujúca jeden riadok v tabuľke s lekciami.
@@ -14,19 +15,36 @@ import {LessonTypeSelectBox} from "components/LessonTypeSelectBox";
  */
 export  const LessonTableRow = ({lesson, actions}) => {
 
+
+    const [order, setOrder] = useState(lesson.order);
+
+
     const onClick = () => {
         const payload = {lesson: lesson}
         actions.onLessonRemove(payload)
         console.log('jdu smazat hodinu')
     }
 
+    const onOrderChange = (event) => {
+        setOrder(Number(event.target.value))
+    };
 
     return (
         <tr>
             <td>{lesson.id}</td>
             <td>{lesson.name}</td>
             <td>{lesson.lastchange}</td>
+            <td>{lesson.order}</td>
             <td><LessonTypeSelectBox lesson={lesson}></LessonTypeSelectBox></td>
+            <td>
+                <input type="number" value={order} onChange={onOrderChange} ></input>
+                <UpdateLessonOrderButton
+                    lessonId={lesson.id}
+                    lastchange={lesson.lastchange}
+                    order={order}
+                    actions={actions}
+                />
+            </td>
             <td><LessonUserSelectBox lesson={lesson}> </LessonUserSelectBox></td>
             <td><LessonFacilitySelectBox lesson={ lesson }> </LessonFacilitySelectBox></td>
             <td>
